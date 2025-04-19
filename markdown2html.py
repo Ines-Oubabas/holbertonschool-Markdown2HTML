be1575bd1863108069a64c02d7ed5f17ea6323dd
Ines-Oubabas <inesoubabas56@gmail.com>
2025-04-19 13:12:53 +0200

    tester 0

--- markdown2html.py
+++ markdown2html.py
@@ -6,6 +6,7 @@ Markdown to HTML converter - Task 0 only
 import sys
 import os
 
+# Vérifier le nombre d'arguments
 if len(sys.argv) < 3:
     print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
     sys.exit(1)
@@ -13,14 +14,17 @@ if len(sys.argv) < 3:
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 
+# Vérifier si le fichier markdown d'entrée existe
 if not os.path.isfile(input_file):
     print(f"Missing {input_file}", file=sys.stderr)
     sys.exit(1)
 
+# Lecture et écriture
 with open(input_file, 'r') as f_in:
     content = f_in.read()
 
 with open(output_file, 'w') as f_out:
     f_out.write(content)
 
+# Sortie silencieuse
 sys.exit(0)
